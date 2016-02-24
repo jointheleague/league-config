f428d458b44b82a3df67fcdbabf50d09cc65f20c
Trey Hunner <trey@treyhunner.com>
2016-02-24 14:16:22 -0800

    Update clear everything config script

--- clear_everything.sh
+++ clear_everything.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
+open /Applications/Google\ Drive\ 2.app/
+
 # Kill GitHub, delete user, reopen
 killall GitHub
 rm -rf ~/Library/Application\ Support/GitHub\ for\ Mac/*
-open /Applications/GitHub.app/
+rm -rf ~/.git*
 
-
-# Kill Eclipse, delete projects
+# Kill Eclipse, delete projects, keep metadata
 killall eclipse
-rm -rf ~/workspace/*
-rm -rf ~/workspace/.metadata/.plugins/org.eclipse.core.resources
+cd ~/Documents/workspace/
 
+shopt -s extglob
+rm -rf !(.metadata)
+
+rm -rf ~/Documents/workspace/.metadata/.plugins/org.eclipse.core.resources
+
+rm -rf ~/Documents/workspace/.metadata/.plugins/org.eclipse.e4.workbench
 
 # Delete everything from Desktop and Downloads
-rm -rf ~/Desktop/* ~/Downloads/* ~/Documents/*
+rm -rf ~/Desktop/* ~/Downloads/* 
+
+cd ~/Documents/
+rm -rf !(workspace)
+
+mkdir ~/Documents/ProcessingSketches
 
 killall "Google Chrome"
